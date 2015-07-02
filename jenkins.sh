f307de89cdc4314e9620ec6cc7fd898e7b1c8e59
Rafael Ximenes <rmalveis@gmail.com>
2015-07-02 18:32:14 -0300

    Automating mysql database creation for builds

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# check if a mysql container is linked.
+if [ $MYSQL_PORT_3306_TCP_ADDR != "" ]; then
+   mysql -uroot -pmysql -h${MYSQL_PORT_3306_TCP_ADDR} < /tmp/init-base.sql
+fi
+
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
